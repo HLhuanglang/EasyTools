6388ba9af9c9f5d752e7e82214e8388ceaf49a21
黄浪 <1282424466@qq.com>
2022-07-18 13:20:30 +0800

    <update>更新工程文件，修改库导出头文件

---
+++ src/easy_tools.cpp
@@ -0,0 +1,9 @@
+#include "easy_tools.h"
+
+bool et_init() {
+	return true;
+}
+
+bool et_exit() {
+	return true;
+}
--- src/easy_tools.h
+++ src/easy_tools.h
@@ -1,7 +1,16 @@
 #ifndef __EASY_TOOLS_H
 #define __EASY_TOOLS_H
 
+#include "prefix/prefix.h"
 #include "log/log.h"
 #include "singleton/sigleton.h"
+#include "serialize/ini/ini.h"
+
+__et_extern_c_enter
+
+__et_export bool et_init();
+__et_export bool et_exit();
+
+__et_extern_c_leave
 
 #endif // !__EASY_TOOLS_H
