8c9746f84b5bcfff6de5fc2923211a46d0bbf262
Holo_wo <1282424466.com>
2022-07-25 19:15:01 +0800

    <add>增加linux编译脚本

---
+++ compile_env/linux/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+function usage {
+    echo -e "use this command line to compile:\033[31m ./build.sh [release|debug] [x86|x64] \033[0m"
+}
+
+if [ $# -lt 2 ];then
+    usage
+    exit -1
+fi
+
+project_dir=`pwd`
+output_dir=${project_dir}/../../output
+lib_dir=${output_dir}/lib
+inc_dir=${output_dir}/include
+
+if [ ! -d ${output_dir} ];then
+    mkdir -p ${output_dir}
+    mkdir -p ${lib_dir}
+    mkdir -p ${inc_dir}
+fi
+
+#编译
+make mode=$1 platform=$2
