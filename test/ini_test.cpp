b2935171629fc5776df9adb52c0768e14d06d033
Holo_wo <1282424466.com>
2022-07-25 19:15:01 +0800

    <add>增加linux编译脚本

--- test/ini_test.cpp
+++ test/ini_test.cpp
@@ -4,12 +4,16 @@
 
 void test_ini() {
 	et_ini_file ini;
+#if defined(__GNUC__)
+	et_ini_handle handle("./test_data/test.ini");
+#else
 	et_ini_handle handle("../test/test_data/test.ini");
+#endif
 	handle.read(ini);
 	auto ret = ini.get_val("section1", "name1");
-	printf("ret=%s", ret.c_str());
+	printf("ret=%s\n", ret.c_str());
 	ret = ini.get_val("section1", "name2");
-	printf("ret=%s", ret.c_str());
+	printf("ret=%s\n", ret.c_str());
 	ret = ini.get_val("section0", "name1");
-	printf("ret=%s", ret.c_str());
+	printf("ret=%s\n", ret.c_str());
 }
